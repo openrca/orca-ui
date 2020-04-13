36fd7827bd4b32c09328bbc96fa57788d7cba9e0
k-jano <kuba.jano3@gmail.com>
2020-04-13 12:56:27 +0200

    Add basic skeleton for react app

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const content = (
+    <div>
+        Main Content1
+    </div>
+)
+
+ReactDOM.render(content, document.getElementById('root'));
