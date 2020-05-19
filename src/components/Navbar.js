6f949d33fc843f87c8db87d72afc38c58c6ef192
Filip Wiechec <f.wiechec@samsung.com>
2020-05-19 13:27:43 +0200

    Style navbar
    * Moved logo from public to src
    * Added logo to navbar
    * Changed Home to OpenRCA in navbar
    * Changed navbar color to darker

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,15 @@
 import React from 'react';
+import logo from './svg/logo.svg';
 import { Navbar, Nav } from 'react-bootstrap';
 
 export class NavbarComponent extends React.Component {
   render() {
     return(
-      <Navbar bg="primary" variant="dark" expand="lg">
-        <Navbar.Brand href="/">Home</Navbar.Brand>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Navbar.Brand href="/">
+          <img src={logo} className="Orca-logo" alt="logo" />
+          OpenRCA
+        </Navbar.Brand>
         <Navbar.Toggle aria-contols="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
