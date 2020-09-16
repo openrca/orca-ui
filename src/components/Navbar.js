69cb8dd42c167c2055eeb93ed51cec9eeb739d04
k-jano <kuba.jano3@gmail.com>
2020-09-16 15:50:02 +0200

    Add alerts count badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,36 @@
 import React from 'react';
 import logo from './svg/logo.svg';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Badge } from 'react-bootstrap';
+import axios from 'axios';
+
 import './Navbar.scss';
 
 export class NavbarComponent extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      alertCount: null
+    }
+    this.alertCountInterval = this.alertCountInterval.bind(this);
+  }
+
+  componentDidMount() {
+    this.alertCountInterval();
+    setInterval(this.alertCountInterval, 30000);
+  }
+
+  alertCountInterval() {
+    axios.get(process.env.REACT_APP_BACKEND_HOST + '/v1/alerts')
+      .then((response) => {
+        this.setState({
+          alertCount: response.data.length,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
   render() {
     return (
       <Navbar bg="dark" variant="dark" expand="lg">
@@ -15,7 +42,10 @@ export class NavbarComponent extends React.Component {
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
             <Nav.Link href="/graph">Graph</Nav.Link>
-            <Nav.Link href="/alerts">Alerts</Nav.Link>
+            <Nav.Link href="/alerts">
+                Alerts
+                <Badge className="alertBadge" variant="danger">{this.state.alertCount ? this.state.alertCount : null}</Badge>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
